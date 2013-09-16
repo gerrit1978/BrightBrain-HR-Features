79f67ff3f244d680c84afd9a477f6945db89a8ea
Gerrit Vos <gevo27@gmail.com>
2013-09-16 22:52:50 +0200

    Job: LinkedIn block

---
+++ bb_hr_job/bb_hr_job_apply_linkedin.tpl.php
@@ -0,0 +1,10 @@
+<div class="button-linkedin" id="buttonLinkedin">   
+  <div class="linkedin-script"  id="linkedinScript">
+    <script src="http://platform.linkedin.com/in.js" type="text/javascript">api_key: 1e85amyd6xmc </script>          
+    <script type="IN/Apply" data-companyname="BrightBrain" 
+            data-jobtitle="<?php print $jobtitle; ?>"
+            data-themecolor="#111" data-email="testing@brightbrain.be">
+      </script>
+  </div>
+  <div class="linkedin-title">Apply with LinkedIn</div>
+</div>
