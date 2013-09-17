1efd929340022be9c7d5641579d6e583e2178fb0
Gerrit Vos <gevo27@gmail.com>
2013-09-17 09:18:32 +0200

    LinkedIn block settings

--- bb_hr_job/bb_hr_job_apply_linkedin.tpl.php
+++ bb_hr_job/bb_hr_job_apply_linkedin.tpl.php
@@ -1,9 +1,9 @@
 <div class="button-linkedin" id="buttonLinkedin">   
   <div class="linkedin-script"  id="linkedinScript">
-    <script src="http://platform.linkedin.com/in.js" type="text/javascript">api_key: 1e85amyd6xmc </script>          
-    <script type="IN/Apply" data-companyname="BrightBrain" 
+    <script src="http://platform.linkedin.com/in.js" type="text/javascript">api_key: <?php print $api_key; ?> </script>          
+    <script type="IN/Apply" data-companyname="<?php print $company_name; ?>" 
             data-jobtitle="<?php print $jobtitle; ?>"
-            data-themecolor="#111" data-email="testing@brightbrain.be">
+            data-themecolor="#111" data-email="<?php print $company_email; ?>">
       </script>
   </div>
   <div class="linkedin-title">Apply with LinkedIn</div>
